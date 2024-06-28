8514ce97d904836e48bcbd9779f87b3b5ccbb38c
Mohab <Mohabeffat76@gmail.com>
2024-06-28 23:55:17 +0300

    Assignment 04

---
+++ Session 04/Task/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////
+            int[] arr = { 16, 265, 31, 54, 25, 66, 87, 800, 19, 11 };
+            int[,] matrix =
+            {
+                {10,20,30 },
+                {40,50,60 },
+                {70,80,90 },
+            };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+            ////////////////////////////
+            ///
+            int max = 0, min = 0;
+            max = min = arr[0];
+            for (int i = 1; i < arr.Length; i++) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            ////////////////////////////
+            ///
+            int SumArr = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++) {
+                for (int j = 0; j < matrix.GetLength(1); j++) {
+                    if (i == j || i +j == matrix.GetLength(0) -1 )
+                        SumArr += matrix[i, j];
+                }
+            }
+            Console.WriteLine(SumArr);
+        }
+
+    }
+}
