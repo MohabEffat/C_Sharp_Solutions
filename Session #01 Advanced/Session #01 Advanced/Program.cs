2386cd56638962b827b0ab151b8adf3fec719114
Mohab <Mohabeffat76@gmail.com>
2024-07-21 23:52:42 +0300

    Session #1 Advanced

---
+++ Session #01 Advanced/Session #01 Advanced/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+
+namespace Session__01_Advanced
+{
+    internal class Program
+    {
+        public static void ReverseArrayList(ArrayList arrayList)
+        {
+            ArrayList temp = new ArrayList();
+            for (int i = arrayList.Count - 1; i >= 0; i--)
+            {
+                temp.Add(arrayList[i]);
+            }
+            arrayList.Clear();
+            for (int i = 0; i < temp.Count; i++)
+            {
+                arrayList.Add(temp[i]);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Range<int> range = new Range<int>(5,10);
+            //Console.WriteLine(range.IsInRange(11));
+            //Console.WriteLine(range.IsInRange(6));
+            //Console.WriteLine(range.Length());
+            #endregion
+
+            #region Q2
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(2);
+            arrayList.Add("mohab");
+            arrayList.Add('x');
+            arrayList.Add('y');
+            arrayList.Add("AAA");
+            arrayList.Add(85);
+            arrayList.Add(987);
+            Console.WriteLine("Before Reverse :");
+            foreach (var i in arrayList)
+            {
+                Console.Write(i + " ");
+            }
+
+            ReverseArrayList(arrayList);
+
+            Console.WriteLine("\nAfter Reverse :");
+            foreach (var i in arrayList)
+            {
+                Console.Write(i + " ");
+            }
+            #endregion
+
+            #region Q3
+            //List<int> list = new List<int>();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    list.Add(i);
+
+            //}
+            //List<int> newList = new List<int>();
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    if (list[i] % 2 == 0)
+            //        newList.Add(list[i]);
+            //}
+            //foreach (var item in newList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+        }
+
+    }
+}
---
+++ Session #01 Advanced/Session #01 Advanced/Range.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session__01_Advanced
+{
+    public class Range<T> where T : struct, IComparable<T>
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+        public Range(T min, T max)
+        { 
+            Min = min; Max = max;
+        }
+
+        public bool IsInRange(T _data)
+        {
+            return _data.CompareTo(Min) > 0 && _data.CompareTo(Max) < 0;
+        }
+        public int Length()
+        {
+            int min = Convert.ToInt32(Min);
+            int max = Convert.ToInt32(Max);
+            return max - min + 1;
+        }
+
+    }
+}
